1a85b284df6ba6718cc1a75e904bc0000bfd27e3
Vesper Yang <43669090+Vesper-Yang@users.noreply.github.com>
2024-09-19 15:43:43 +0800

    创建网页顶部导航Navigation

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,8 @@
 <script setup>
+import BaseNavigation from './components/BaseNavigation.vue';
 </script>
 
 <template>
-    <p>This is a default layout</p>
+    <BaseNavigation />
     <slot />
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,8 @@
 <script setup>
-  definePageMeta({
-    layout: 'login'
-  })
+
 </script>
 
 <template>
   <h1>Home Page</h1>
+  
 </template>
